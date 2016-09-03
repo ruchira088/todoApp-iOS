5e12a9823f4bb3f02a2ba75d4a796e1fc8be4aa2
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2016-09-03 17:31:50 +1000

    Initial Commit

---
+++ Todo App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo App
+//
+//  Created by Ruchira Jayasekara on 3/09/2016.
+//  Copyright © 2016 Ruchira Jayasekara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
