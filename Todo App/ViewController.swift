773ac84ff1aa312984fc3099173700e4006476d7
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2016-09-03 18:40:31 +1000

    Created the login screen

--- Todo App/ViewController.swift
+++ Todo App/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLoginTouchUp(sender: UIButton)
+    {
+        let username: String! = usernameField.text
+        let password: String! = passwordField.text
+        
+        let body = ["username": username, "password": password]
+        
+        do {
+            let jsonBody = try NSJSONSerialization.dataWithJSONObject(body, options: .PrettyPrinted)
+            
+            let url = NSURL(string: "http://52.63.69.15:8000/account/login")
+            
+            let request = NSMutableURLRequest(URL: url!)
+            request.HTTPMethod = "POST"
+            request.setValue("application/json; chartset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = jsonBody
+
+            let requestTask = NSURLSession.sharedSession().dataTaskWithRequest(request){data, response, error in
+                
+                if error != nil {
+                    print(error)
+                    return
+                }
+                
+                do {
+                    let responseData = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! [String:String]
+                    print(responseData["token"]!)
+                }
+                catch
+                {
+                    
+                }
+            }
+            
+            requestTask.resume()
+            
+        } catch
+        {
+            
+        }
+    }
 
 }
 
